6adcdd146d7f35b981aeae820b245be43c26c49e
Johannes Kropf <46578064+johanneskropf@users.noreply.github.com>
2021-01-26 14:09:39 +0100

    Initial commit

---
+++ wakeword.js
@@ -0,0 +1,122 @@
+/**
+ * Copyright 2021 Johannes Kropf
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    
+    const wakewordDetector = require("node-personal-wakeword");
+    const fs = require("fs"); 
+    
+    function WakeWordNode (config) {
+        RED.nodes.createNode(this,config);
+        this.statusTimer = false;
+        this.statusTimer2 = false;
+        this.wakeWordConfig = RED.nodes.getNode(config.wakeword);
+        
+        var node = this;
+        
+        node.wakeWordConfig.files.forEach(file => {
+            node.warn(fs.lstatSync(file).isFile());
+        });
+        
+        function node_status(state1 = [], timeout = 0, state2 = []){
+            
+            if (state1.length !== 0) {
+                node.status({fill:state1[1],shape:state1[2],text:state1[0]});
+            } else {
+                node.status({});
+            }
+            
+            if (node.statusTimer !== false) {
+                clearTimeout(node.statusTimer);
+                node.statusTimer = false;
+            }
+            
+            if (timeout !== 0) {
+                node.statusTimer = setTimeout(() => {
+                
+                    if (state2.length !== 0) {
+                        node.status({fill:state2[1],shape:state2[2],text:state2[0]});
+                    } else {
+                        node.status({});
+                    }
+                    
+                    node.statusTimer = false;
+                    
+                },timeout);
+            }
+        }
+        
+        async function startDetector () {
+            
+            node.detector = new wakewordDetector ({
+                threshold: 0.5 // Default value
+            });
+            
+            await node.detector.addKeyword("test", node.wakeWordConfig.files, {
+                disableAveraging: false, 
+                threshold: 0.6
+            });
+            
+            node.detector.on('ready', () => {
+                node_status(["listening...","blue","dot"]);
+            })
+            
+            node.detector.on('error', err => {
+                node_status(["error","red","dot"],1500);
+                node.error(err);
+            });
+            
+            node.detector.on('keyword', ({keyword, score, threshold, timestamp}) => {
+                let payload = {};
+                payload.keyword = keyword;
+                payload.timestamp = timestamp;
+                payload.score = score;
+                payload.threshold = threshold;
+                node.send({payload:payload});
+                node_status(["keyword detected","green","dot"]);
+            });
+        }
+        
+        function writeChunk(chunk){
+            
+            try {
+                node.detector.write(chunk);
+            }
+            catch (error){
+                node.error(error);
+            }
+            return;
+            
+        }
+        
+        this.on('input', function(msg, send, done) {
+            
+            if (Buffer.isBuffer(msg.payload)) {
+                if (!node.detector) {
+                    node_status(["starting detector","blue","ring"]);
+                    startDetector();
+                } else {
+                    writeChunk(msg.payload);
+                }
+            }
+            if (done) { done(); }
+            
+        });
+    
+    }
+    
+    RED.nodes.registerType("wake-word",WakeWordNode);
+}
